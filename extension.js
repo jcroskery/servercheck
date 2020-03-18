545c87cf876113f56a479e8def52de7cb3372209
Justus Croskery <justus@olmmcc.tk>
2020-03-17 20:48:43 -0400

    Initial commit

---
+++ extension.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+
+function init() {
+    log('initializing ServerCheck');
+}
+
+function enable() {
+    log('enabling ServerCheck');
+}
+
+function disable() {
+    log('disabling ServerCheck');
+}
---
+++ prefs.js
@@ -0,0 +1,76 @@
+"use strict";
+
+const { GObject, Gtk, GLib } = imports.gi;
+
+// It's common practice to keep GNOME API and JS imports in separate blocks
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+
+
+// Like `extension.js` this is used for any one-time setup like translations.
+function init() {
+    log("Initializing ServerCheck Preferences");
+}
+
+
+// This function is called when the preferences window is first created to build
+// and return a Gtk widget. As an example we'll create and return a GtkLabel.
+function buildPrefsWidget() {
+    const prefs = new Prefs();
+    prefs.show_all();
+    return prefs;
+}
+
+var Prefs = GObject.registerClass(class extends Gtk.Box {
+    _init(params) {
+        super._init(params);
+        this.margin = 24;
+        this.orientation = Gtk.Orientation.VERTICAL;
+
+        let server = "https://www.olmmcc.tk";
+        let timer = 60;
+
+        const hbox1 = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
+        const setting1_label = this._addLabel(hbox1, "Check every " + timer + " seconds: ");
+        const adjustment = new Gtk.Adjustment({
+            lower: 60,
+            upper: 1800,
+            step_increment: 1
+        });
+        const setting_slider = new Gtk.HScale({
+            digits: 0,
+            adjustment: adjustment,
+            value_pos: Gtk.PositionType.RIGHT
+        });
+        setting_slider.set_value(timer);
+        setting_slider.connect('value-changed', button => {
+            let i = Math.round(button.get_value()).toString();
+            setting1_label.label = "Check every " + i + " seconds: ";
+            //Set new timer
+        });
+        hbox1.pack_end(setting_slider, true, true, 0);
+        this.add(hbox1);
+        const hbox2 = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
+        const setting2_label = this._addLabel(hbox2, "Server url: " + server);
+        let entryBuffer = Gtk.EntryBuffer.new(server, server.length);
+        const url = Gtk.Entry.new_with_buffer(entryBuffer);
+        const button = Gtk.Button.new_with_label("Change Server");
+        hbox2.pack_end(button, true, true, 0);
+        button.connect('clicked', () => {
+            let new_server = url.get_buffer().get_text();
+            setting2_label.label = "Server url: " + new_server;
+            //Set new server
+        });
+        hbox2.pack_end(url, true, true, 0);
+        this.add(hbox2)
+    }
+    _addLabel(hbox, label) {
+        const setting_label = new Gtk.Label({
+            label: label,
+            xalign: 0,
+            use_markup: true
+        });
+        hbox.pack_start(setting_label, true, true, 0);
+        return setting_label;
+    }
+});
