be955439a45e443a86a8ee4c68d56b2eca2166ba
Justus Croskery <justus@olmmcc.tk>
2020-03-18 14:05:52 -0400

    Fix notifications

--- extension.js
+++ extension.js
@@ -4,10 +4,10 @@ const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Conf = Me.imports.conf.Conf;
 const {Gio, GLib} = imports.gi;
 const Soup = imports.gi.Soup;
-const Sess = new Soup.SessionAsync();
 const MessageTray = imports.ui.messageTray;
 const Source = MessageTray.Source;
 const Main = imports.ui.main;
+const Util = imports.misc.util;
 
 function init() {
     log('initializing ServerCheck');
@@ -24,6 +24,7 @@ function disable() {
 class Extension {
     constructor() {
         log('enabling ServerCheck');
+        this.notificationShown = false;
         this.config = new Conf();
         this.checkMailTimeout = null;
         this.startTimeout();
@@ -39,22 +40,30 @@ class Extension {
         });
     }
     _showNotification() {
-        let source = new Source("ServerCheck", "dialog-error");
-        Main.messageTray.add(source);
-        let notification = new MessageTray.Notification(source, "Server Error", "Failed to connect to " + this.config.getServer(), {
-            gicon: new Gio.ThemedIcon({ name: "dialog-error"})
-        });
-        notification.connect('activated', () => {
-            this._openBrowser(this.config.getServer());
-        });
-        source.showNotification(notification);
+        if (!this.notificationShown) {
+            let source = new Source("ServerCheck", "dialog-error");
+            Main.messageTray.add(source);
+            let notification = new MessageTray.Notification(source, "Server Error", "Failed to connect to " + this.config.getServer(), {
+                gicon: new Gio.ThemedIcon({ name: "dialog-error" })
+            });
+            notification.connect('activated', () => {
+                const defaultBrowser = Gio.app_info_get_default_for_uri_scheme("http").get_executable();
+                Util.trySpawnCommandLine(defaultBrowser + " " + this.config.getServer());
+            });
+            source.showNotification(notification);
+            this.notificationShown = true;
+        }
     }
     _checkServer() {
         log('Checking server');
         const msg = Soup.Message.new("GET", this.config.getServer());
+        let Sess = new Soup.SessionAsync(); 
+        Sess.timeout = 10;
         Sess.queue_message(msg, (_, msg) => {
-            if (msg.status_code !== 200) {
+            if (!msg || msg.status_code !== 200) {
                 this._showNotification();
+            } else {
+                this.notificationShown = false;
             }
         });
     }
